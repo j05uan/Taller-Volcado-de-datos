6801e19ce17b4f538a3d958564d9df96cb55f7c4
Juan Pablo Lopez Amaya <152333487+j05uan@users.noreply.github.com>
2024-08-01 15:27:13 -0500

    Create DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,33 @@
+
+CREATE DATABASE mi_base_de_datos;
+
+\c mi_base_de_datos
+-- Crear la tabla de países
+  
+CREATE TABLE pais (
+    id_pais SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULLs
+);
+
+-- Crear la tabla de departamentos
+CREATE TABLE departamento (
+    id_departamento SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    id_pais INT REFERENCES pais(id_pais) ON DELETE CASCADE
+);
+
+-- Crear la tabla de municipios
+CREATE TABLE municipio (
+    id_municipio SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    id_departamento INT REFERENCES departamento(id_departamento) ON DELETE CASCADE
+);
+
+CREATE TABLE localidad (
+    id_localidad SERIAL PRIMARY KEY,
+    id_departamento INT REFERENCES departamento(id_departamento) ON DELETE CASCADE,
+    id_municipio INT REFERENCES municipio(id_municipio) ON DELETE CASCADE
+);
+
+COPY localidad (id_departamento, id_municipio)
+FROM '/ruta/a/tu/localidades.csv' DELIMITER ',' CSV HEADER;
